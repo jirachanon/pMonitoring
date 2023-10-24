919d87b6aee1c5aa9bf9a7242863c91a6ec73a6f
jirachanon <110511312+jirachanon@users.noreply.github.com>
2023-10-25 02:21:32 +0700

    svg icon updated

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -30,21 +30,11 @@
           <v-col cols="12" class="gender-group d-flex">
             <p>Gender</p>
             <div class="circle" :class="form.gender == 1 ? 'active' : ''" @click="chooseGender(1)">
-              <svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="35" height="46" fill="currentColor"
-                class="bi bi-gender-female" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                  d="M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5z"
-                  fill="white"></path>
-              </svg>
+              <icons-female />
             </div>
             <p>เพศหญิง</p>
             <div class="circle" :class="form.gender == 2 ? 'active' : ''" @click="chooseGender(2)">
-              <svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="35" height="46" fill="currentColor"
-                class="bi bi-gender-male" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                  d="M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2H9.5zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8z"
-                  fill="white"></path>
-              </svg>
+                <icons-male />
             </div>
             <p>เพศชาย</p>
           </v-col>
